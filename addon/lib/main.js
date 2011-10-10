05b3355ee0a3ba01c87a591fddc5a4704e95f137
Jeff Griffiths <jeff@canuckistani.ca>
2011-10-10 16:42:22 -0700

    added a widget

--- addon/lib/main.js
+++ addon/lib/main.js
@@ -1,5 +1,6 @@
 var data = require("self").data;
 var pageWorkers = require("page-worker");
+var tabs = require("tabs");
 
 var worker = pageWorkers.Page({
     contentUrl: data.url('worker.html'),
@@ -12,4 +13,13 @@ worker.on('message', function(m) {
 
 worker.on('error', function(err) {
     console.log(err);
+});
+
+require("widget").Widget({
+    id: "mozilla-icon",
+    label: "Open the Websocket Client Page",
+    contentURL: "http://www.mozilla.org/favicon.ico",
+    onClick: function() {
+        tabs.open(data.url("client.html"))
+    }
 });
